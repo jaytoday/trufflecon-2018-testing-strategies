ac668a5e35343255a8a8b6633555e1f024a46da9
Chris Smith <me@chrisryansmith.com>
2018-10-05 11:53:34 -0700

    encapsulate tests to eliminate sharing state

--- test/SimpleToken.test.js
+++ test/SimpleToken.test.js
@@ -8,106 +8,123 @@ contract('SimpleToken', function(accounts) {
   const investorTooFar = accounts[3];
   const initialSupply = 10000;
 
-  before(async () => {
-    this.capValidator = await CapValidator.deployed();
-    this.simpleToken = await SimpleToken.deployed();
-  });
+  let capValidator;
+  let simpleToken;
+  
+  beforeEach(async () => {
+    capValidator = await CapValidator.new({from: owner});
+    simpleToken = await SimpleToken.new(capValidator.address, {from:owner});
+  })
 
   it('should assign the INITIAL_SUPPLY to the owner', async () => {
-    this.ownerBalance = await this.simpleToken.balanceOf.call(owner);
+    this.ownerBalance = await simpleToken.balanceOf.call(owner);
     assert.equal(this.ownerBalance.toNumber(), initialSupply);
   });
 
   it('should initially have an investorCount of 1', async () => {
-    this.investorCount = await this.capValidator.investorCount.call();
+    this.investorCount = await capValidator.investorCount.call();
     assert.equal(this.investorCount.toNumber(), 1);
   });
 
-  it('should allow the owner to transfer to another address', async () => {
+  it('should allow the owner to transfer to another address and increase investorCount', async () => {
     const amount = 100;
 
-    await this.simpleToken.transfer(alice, amount, {from:owner});
+    await simpleToken.transfer(alice, amount, {from:owner});
 
-    this.ownerBalance = await this.simpleToken.balanceOf.call(owner);
-    assert.equal(this.ownerBalance.toNumber(), initialSupply - amount);
+    const ownerBalance = await simpleToken.balanceOf.call(owner);
+    assert.equal(ownerBalance.toNumber(), initialSupply - amount);
 
-    this.aliceBalance = await this.simpleToken.balanceOf.call(alice);
-    assert.equal(this.aliceBalance.toNumber(), amount);
+    const aliceBalance = await simpleToken.balanceOf.call(alice);
+    assert.equal(aliceBalance.toNumber(), amount);
+    
+    const investorCount = await capValidator.investorCount.call();
+    assert.equal(investorCount.toNumber(), 2);
   });
 
-  it('should have an investorCount of 2 after new transfer', async () => {
-    this.investorCount = await this.capValidator.investorCount.call();
-    assert.equal(this.investorCount.toNumber(), 2);
-  });
-
-  it('should allow a non-owner to transfer to another new address', async () => {
+  it('should allow a non-owner to transfer to another new address and increase investorCount', async () => {
     const amount = 10;
-    const alicePreBalance = this.aliceBalance;
+    await simpleToken.transfer(alice, amount * 2, {from:owner});
 
-    await this.simpleToken.transfer(bob, amount, {from:alice});
+    const preInvestorCount = await capValidator.investorCount.call();
+    assert.equal(preInvestorCount.toNumber(), 2);
+    
+    const alicePreBalance = await simpleToken.balanceOf.call(alice);
+    assert.isAbove(alicePreBalance.toNumber(), amount);
 
-    this.aliceBalance = await this.simpleToken.balanceOf.call(alice);
-    assert.equal(this.aliceBalance.toNumber(), alicePreBalance - amount);
-
-    this.bobBalance = await this.simpleToken.balanceOf.call(bob);
-    assert.equal(this.bobBalance.toNumber(), amount);
+    await simpleToken.transfer(bob, amount, {from:alice});
+    
+    const aliceBalance = await simpleToken.balanceOf.call(alice);
+    assert.equal(aliceBalance.toNumber(), alicePreBalance - amount);
+    
+    const bobBalance = await simpleToken.balanceOf.call(bob);
+    assert.equal(bobBalance.toNumber(), amount);
+    
+    const investorCount = await capValidator.investorCount.call();
+    assert.equal(investorCount.toNumber(), 3);
   });
 
-  it('should have an investorCount of 3 after new transfer', async () => {
-    this.investorCount = await this.capValidator.investorCount.call();
-    assert.equal(this.investorCount.toNumber(), 3);
-  });
+  describe('when investorCount === CAP', () => {
+    beforeEach(async () => {
+      const amount = 10;
+      const CAP = await capValidator.CAP();
+    
+      let alicePreBalance = await simpleToken.balanceOf.call(alice);
+      let bobPreBalance = await simpleToken.balanceOf.call(bob);
+      let investorCount = await capValidator.investorCount.call();
+      assert.equal(investorCount.toNumber(), 1);
+      
+      await simpleToken.transfer(alice, amount, {from:owner});
+      alicePreBalance = await simpleToken.balanceOf.call(alice);
+      assert.equal(alicePreBalance.toNumber(), amount);
 
-  it('should allow a transfer to not new address', async () => {
-    const amount = 10;
-    const alicePreBalance = this.aliceBalance;
-    const bobPreBalance = this.bobBalance;
+      await simpleToken.transfer(bob, amount, {from:owner});
+      bobPreBalance = await simpleToken.balanceOf.call(bob);
+      assert.equal(bobPreBalance.toNumber(), amount);
 
-    await this.simpleToken.transfer(bob, amount, {from:alice});
+      investorCount = await capValidator.investorCount.call();
+      assert.equal(investorCount.toNumber(), CAP.toNumber());
+    });
 
-    this.aliceBalance = await this.simpleToken.balanceOf.call(alice);
-    assert.equal(this.aliceBalance.toNumber(), alicePreBalance - amount);
+    it('should allow a transfer to not new address when at CAP', async () => {
+      const amount = 10;
+      const alicePreBalance = await simpleToken.balanceOf.call(alice);
+      const bobPreBalance = await simpleToken.balanceOf.call(bob);
+      
+      await simpleToken.transfer(bob, amount, {from:alice});
+  
+      const aliceBalance = await simpleToken.balanceOf.call(alice);
+      assert.equal(aliceBalance.toNumber(), alicePreBalance.toNumber() - amount);
+  
+      const bobBalance = await simpleToken.balanceOf.call(bob);
+      assert.equal(bobBalance.toNumber(), bobPreBalance.toNumber() + amount);
+    });
+    it('should not allow a transfer to a new address', async () => {
+      const amount = 10;
+      const ownerBalance = await simpleToken.balanceOf.call(owner);
+      assert.isAbove(ownerBalance.toNumber(), amount);
 
-    this.bobBalance = await this.simpleToken.balanceOf.call(bob);
-    assert.equal(this.bobBalance.toNumber(), bobPreBalance.toNumber() + amount);
-  });
+      try {
+        await simpleToken.transfer(investorTooFar, amount, {from:owner});
+        assert.isFalse(true, 'Transfer should throw error');
+      } catch(err) {
+        assert.isTrue(
+          err.toString().includes('revert'),
+          `Error thrown did not contain "revert" - ${err.toString()}`);
+      }
+    });
+    it('should allow a transfer of full balance', async () => {
+      const alicePreBalance = await simpleToken.balanceOf.call(alice);
+  
+      await simpleToken.transfer(investorTooFar, alicePreBalance, {from:alice});
+  
+      const aliceBalance = await simpleToken.balanceOf.call(alice);
+      assert.equal(aliceBalance.toNumber(), 0);
+  
+      const investorTooFarBalance = await simpleToken.balanceOf.call(investorTooFar);
+      assert.equal(investorTooFarBalance.toNumber(), alicePreBalance.toNumber());
 
-  it('should still have an investorCount of 3 after not-new transfer', async () => {
-    this.investorCount = await this.capValidator.investorCount.call();
-    assert.equal(this.investorCount.toNumber(), 3);
-  });
-
-  it('should not allow a transfer to a new address if at investorCap', async () => {
-    const amount = 10;
-    const alicePreBalance = this.aliceBalance;
-    const CAP = await this.capValidator.CAP();
-    assert.equal(this.investorCount.toNumber(), CAP.toNumber());
-
-    try {
-      await this.simpleToken.transfer(investorTooFar, this.ownerBalance, {from:owner});
-      assert.isFalse(true, 'Transfer should throw error');
-    } catch(err) {
-      assert.isTrue(
-        err.toString().includes('revert'),
-        `Error thrown did not contain "revert" - ${err.toString()}`);
-    }
-  });
-
-  it('should allow a transfer of full balance', async () => {
-    const alicePreBalance = this.aliceBalance;
-    const bobPreBalance = this.bobBalance;
-
-    await this.simpleToken.transfer(bob, alicePreBalance, {from:alice});
-
-    this.aliceBalance = await this.simpleToken.balanceOf.call(alice);
-    assert.equal(this.aliceBalance.toNumber(), 0);
-
-    this.bobBalance = await this.simpleToken.balanceOf.call(bob);
-    assert.equal(this.bobBalance.toNumber(), bobPreBalance.toNumber() + alicePreBalance.toNumber());
-  });
-
-  it('should still have an investorCount of 2 after transfer to 0', async () => {
-    this.investorCount = await this.capValidator.investorCount.call();
-    assert.equal(this.investorCount.toNumber(), 2);
+      const investorCount = await capValidator.investorCount.call();
+      assert.equal(investorCount.toNumber(), 3);
+    });
   });
 });
