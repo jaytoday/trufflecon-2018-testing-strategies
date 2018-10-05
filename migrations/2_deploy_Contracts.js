0c871fca2e8b789b7622b058bf0578d91ecd1d5c
Chris Smith <me@chrisryansmith.com>
2018-10-05 11:54:17 -0700

    remove unneeded migrations

--- migrations/2_deploy_Contracts.js
+++
@@ -1,8 +0,0 @@
-const CapValidator = artifacts.require('CapValidator');
-const SimpleToken = artifacts.require('SimpleToken');
-
-module.exports = async function(deployer) {
-  await deployer.deploy(CapValidator)
-    .then(() => CapValidator.deployed())
-    .then(() => deployer.deploy(SimpleToken, CapValidator.address));
-};
